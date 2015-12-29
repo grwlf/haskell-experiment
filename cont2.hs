1b8ce4c59cffc7c991b450aecc57027987306b65
Sergey Mironov <grrwlf@gmail.com>
2015-12-29 21:43:29 +0000

    cont2.hs: simplify monad

--- cont2.hs
+++ cont2.hs
@@ -9,25 +9,23 @@ import Control.Monad.State
 
 data Result t a =
     Fine a
-  | EInt (Int -> t (Result t a) a)
-  | EBool (Bool -> t (Result t a) a)
+  | EInt (Int -> t (Result t a) (Result t a))
+  | EBool (Bool -> t (Result t a) (Result t a))
 
-data S m r = S (r -> m r) | Nil
-
-newtype VKT m r a = VKT { unVKT :: StateT (S (VKT m r) r) (ContT r m) a }
-  deriving(Functor, Applicative, Monad, MonadState (S (VKT m r) r), MonadCont, MonadIO)
+newtype VKT m r a = VKT { unVKT :: StateT (r -> VKT m r r) (ContT r m) a }
+  deriving(Functor, Applicative, Monad, MonadState (r -> VKT m r r), MonadCont, MonadIO)
 
 runVKT :: (Monad m) => VKT m r r -> m r
-runVKT m = runContT (runStateT (unVKT (protect m)) Nil) (return . fst) where
+runVKT m = runContT (runStateT (unVKT (protect m)) undefined) (return . fst) where
   protect :: VKT m r r -> VKT m r r
   protect m = do
     callCC $ \k -> do
-      modify (const (S k))
+      modify (const k)
       m
 
-raise :: (MonadCont m, MonadState (S m a1) m) => ((a -> m b) -> a1) -> m a
+raise :: (MonadCont m, MonadState (a1 -> m a1) m) => ((a -> m b) -> a1) -> m a
 raise z = callCC $ \k -> do
-  (S err) <- get
+  err <- get
   err (z k)
   undefined
 
@@ -40,6 +38,6 @@ test1 = do
                   liftIO $ putStrLn $ "x=" ++ (show x)
                   return (Fine (42::Int))
   putStrLn "error!"
-  (Fine r) <- runVKT (k 33 >>= return . Fine)
+  (Fine r) <- runVKT (k 33)
   putStrLn (show r)
 
