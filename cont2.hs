0120c4965c4fc7b003f1e6bf6927044d13c4b22b
Sergey Mironov <grrwlf@gmail.com>
2015-12-31 12:48:25 +0300

    cont2.hs: explicit catch/raise

--- cont2.hs
+++ cont2.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
 module Cont where
 
 import Control.Monad
@@ -13,31 +15,36 @@ data Result t a =
   | EBool (Bool -> t (Result t a) (Result t a))
 
 newtype VKT m r a = VKT { unVKT :: StateT (r -> VKT m r r) (ContT r m) a }
-  deriving(Functor, Applicative, Monad, MonadState (r -> VKT m r r), MonadCont, MonadIO)
+  deriving(Functor, Applicative, Monad, MonadState (r -> VKT m r r),  MonadCont, MonadIO)
+
+class (MonadCont m, MonadState (r -> m r) m) => MonadVK m r
+
+instance MonadVK (VKT m r) r
 
 runVKT :: (Monad m) => VKT m r r -> m r
-runVKT m = runContT (runStateT (unVKT (protect m)) undefined) (return . fst) where
-  protect :: VKT m r r -> VKT m r r
-  protect m = do
-    callCC $ \k -> do
-      modify (const k)
-      m
+runVKT m = runContT (evalStateT (unVKT (catch m)) undefined) return
 
-raise :: (MonadCont m, MonadState (a1 -> m a1) m) => ((a -> m b) -> a1) -> m a
+catch :: (MonadVK m r) => m r -> m r
+catch m = do
+  callCC $ \k -> do
+    modify (const k)
+    m
+
+raise :: (MonadVK m r) => ((a -> m b) -> r) -> m a
 raise z = callCC $ \k -> do
   err <- get
   err (z k)
   undefined
 
 
-test1 :: IO ()
-test1 = do
-  (EInt k) <- runVKT $ do
-                  liftIO $ putStrLn "p1"
-                  x <- raise EInt
-                  liftIO $ putStrLn $ "x=" ++ (show x)
-                  return (Fine (42::Int))
-  putStrLn "error!"
-  (Fine r) <- runVKT (k 33)
-  putStrLn (show r)
+-- test1 :: IO ()
+-- test1 = do
+--   (EInt k) <- runVKT $ do
+--                   liftIO $ putStrLn "p1"
+--                   x <- raise EInt
+--                   liftIO $ putStrLn $ "x=" ++ (show x)
+--                   return (Fine (42::Int))
+--   putStrLn "error!"
+--   (Fine r) <- runVKT (k 33)
+--   putStrLn (show r)
 
