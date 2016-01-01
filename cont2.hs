e4069d95eae8de8cfde22c246e75cb0dd82dd9d9
Sergey Mironov <grrwlf@gmail.com>
2016-01-01 15:52:24 +0000

    replace StateT with ReadrtT

--- cont2.hs
+++ cont2.hs
@@ -8,45 +8,45 @@ import Control.Monad
 import Control.Monad.Cont
 import Control.Monad.Trans
 import Control.Monad.State
+import Control.Monad.Reader
 
 data Result t a =
     Fine a
   | EInt (Int -> t (Result t a) (Result t a))
   | EBool (Bool -> t (Result t a) (Result t a))
 
-type Guts x m r a = StateT (r -> x m r r) (ContT r m) a
+type Guts x m r a = ReaderT (r -> x m r r) (ContT r m) a
 
 newtype VKT m r a = VKT { unVKT :: Guts VKT m r a }
-  deriving(Functor, Applicative, Monad, MonadState (r -> VKT m r r),  MonadCont, MonadIO)
+  deriving(Functor, Applicative, Monad, MonadReader (r -> VKT m r r),  MonadCont, MonadIO)
 
-class (MonadCont m, MonadState (r -> m r) m) => MonadVK m r
+class (MonadCont m, MonadReader (r -> m r) m) => MonadVK m r
 
 instance MonadVK (VKT m r) r
 
 runVKT :: (Monad m) => VKT m r r -> m r
-runVKT m = runContT (evalStateT (unVKT (catch m)) undefined) return
+runVKT m = runContT (runReaderT (unVKT (catch m)) undefined) return
 
 catch :: (MonadVK m r) => m r -> m r
 catch m = do
   callCC $ \k -> do
-    modify (const k)
-    m
+    local (const k) m
 
 raise :: (MonadVK m r) => ((a -> m b) -> r) -> m a
 raise z = callCC $ \k -> do
-  err <- get
+  err <- ask
   err (z k)
   undefined
 
 
--- test1 :: IO ()
--- test1 = do
---   (EInt k) <- runVKT $ do
---                   liftIO $ putStrLn "p1"
---                   x <- raise EInt
---                   liftIO $ putStrLn $ "x=" ++ (show x)
---                   return (Fine (42::Int))
---   putStrLn "error!"
---   (Fine r) <- runVKT (k 33)
---   putStrLn (show r)
+test1 :: IO ()
+test1 = do
+  (EInt k) <- runVKT $ do
+                  liftIO $ putStrLn "p1"
+                  x <- raise EInt
+                  liftIO $ putStrLn $ "x=" ++ (show x)
+                  return (Fine (42::Int))
+  putStrLn "error!"
+  (Fine r) <- runVKT (k 33)
+  putStrLn (show r)
 
